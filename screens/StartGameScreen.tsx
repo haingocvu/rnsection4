3ae0a2a3a82f0552f44a18eb45154250cadf6d7a
Hai Vu Ngoc (BTS-DL) <hai.vungoc@vib.com.vn>
2023-04-14 14:37:59 +0700

    shadow

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -1,12 +1,12 @@
 import React, {FC, PropsWithChildren} from 'react';
-import {View, TextInput} from 'react-native';
+import {View, TextInput, StyleSheet} from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
 interface IProps extends PropsWithChildren<unknown> {}
 
 const StartGameScreen: FC<IProps> = () => {
   return (
-    <View>
+    <View style={style.container}>
       <TextInput />
       <PrimaryButton>Confirm</PrimaryButton>
       <PrimaryButton>Reset</PrimaryButton>
@@ -15,3 +15,18 @@ const StartGameScreen: FC<IProps> = () => {
 };
 
 export default StartGameScreen;
+
+const style = StyleSheet.create({
+  container: {
+    marginTop: 20,
+    padding: 16,
+    marginHorizontal: 16,
+    backgroundColor: '#72063c',
+    borderRadius: 8,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+});

d4ac56a4b2e32d0fd27d6858f908899cf0f43803
Hai Vu Ngoc (BTS-DL) <hai.vungoc@vib.com.vn>
2023-04-20 17:29:09 +0700

    game over screen p1

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,7 +1,38 @@
-import {Text} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import Title from '../components/ui/Title';
+import {colors} from '../constants/constants';
 
 const GameOverScreen = () => {
-  return <Text>Game over</Text>;
+  return (
+    <View style={styles.wrapper}>
+      <Title>Game over</Title>
+      <View style={styles.imgWrapper}>
+        <Image source={require('../assets/images/success.png')} alt="success" />
+      </View>
+      <Text>Your phone needs x rounds to guess the number Y</Text>
+    </View>
+  );
 };
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  wrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 26,
+    marginTop: 39,
+  },
+  imgWrapper: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 5,
+    overflow: 'hidden',
+    borderColor: colors.primary500,
+  },
+  img: {
+    width: '100%',
+    height: '100%',
+  },
+});
